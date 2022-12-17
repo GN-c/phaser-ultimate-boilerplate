a63a0ebd29ad385cf55cf6cc0c18c0e8e14235c8
GN-c <giorginadiradze1234@gmail.com>
2022-12-17 12:21:59 +0400

    .

--- webpack.config.ts
+++ webpack.config.ts
@@ -37,6 +37,7 @@ const config: webpack.Configuration = {
     new webpack.DefinePlugin({
       "typeof WEBGL_RENDERER": JSON.stringify(true),
       "typeof CANVAS_RENDERER": JSON.stringify(false),
+      "typeof WEBGL_DEBUG": JSON.stringify(false),
       "typeof EXPERIMENTAL": JSON.stringify(false),
       "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
       "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
